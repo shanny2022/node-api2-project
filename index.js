9aa1ca62a78362d856fdd1ba38ef5a0c542e5091
shanny2022 <115324786+shanny2022@users.noreply.github.com>
2023-12-24 22:16:26 -0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it here
+// require your server and launch it here
+const server = require('./api/server')
+const port = 9000
+server.list(port, ()=>{
+    console.log(`\n*** Server running on http://localhost:${port} ***\n`)
+})
